02d9ad00684771336d6b79bdb92c248de0efedf8
ChrisNieves <nievescs20@gmail.com>
2023-01-04 14:10:27 -0500

    removed solution from main branch

--- functions/yourFunctions.js
+++ functions/yourFunctions.js
@@ -1,17 +1,6 @@
-function addNames(studentArray) {
-  const result = [];
-  studentArray.forEach((student) => {
-    result.push(student.name);
-  });
-  return result;
-}
+function addNames() {}
 
-function multiply(value1, value2) {
-  if (typeof value1 !== "number" || typeof value2 !== "number") {
-    return 0;
-  }
-  return value1 * value2;
-}
+function multiply() {}
 
 module.exports = {
   addNames,
